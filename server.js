de8d4cc5f68ac4ddce94f98f4e6c4a33852fc9ed
Eric VR <124469022+ericvalcarcel@users.noreply.github.com>
2024-01-04 00:39:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const axios = require('axios');
 const cors = require('cors');
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 10000;
 
